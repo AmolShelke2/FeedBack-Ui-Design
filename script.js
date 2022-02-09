3b83e8e21e91fe0d7ef66374bcfbc8d55c247273
Amol shelke <shelkeamol897@gmail.com>
2022-02-09 17:23:17 +0530

    Selected rating erro

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const ratings = document.querySelectorAll(".rating");
 const ratingsContainer = document.querySelector(".ratings-container");
 const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
-let selectedRating = "satisfied";
+let selectedRating = "Satisfied";
 
 ratingsContainer.addEventListener("click", function (e) {
   if (
